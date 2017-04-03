32f41cd5f2830541c88bddbabf2bf9fd04ada2c2
pearpages <pere@soms.cat>
2017-04-03 16:59:22 +0200

    react components

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -3,11 +3,20 @@ import map from 'lodash/map';
 import './Restaurant.css';
 
 class Restaurant extends Component {
-  render () {
-    const { name } = this.props;
+  render() {
+    const { name, votes, handleSelect, handleDeselect } = this.props;
     return (
       <article className="Restaurant">
-        <h3>{ name }</h3>
+        <h3>{name}</h3>
+        <ul>
+          { votes && map(votes, (vote,key) => <li key={key}>{vote}</li>)}
+        </ul>
+        <button onClick={handleSelect}>
+          Yea, I'd go there
+        </button>
+        <button className="destructive" onClick={handleDeselect}>
+          Nah, nevermind
+        </button>
       </article>
     );
   }
--- src/Restaurants.js
+++ src/Restaurants.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import Restaurant from './Restaurant';
 import map from 'lodash/map';
+import { database } from './firebase';
 import './Restaurants.css';
 
 class Restaurants extends Component {
@@ -8,12 +9,37 @@ class Restaurants extends Component {
     super(props);
   }
 
+  handleSelect(key) {
+    const currentUser = this.props.user;
+    database.ref('/restaurants')
+      .child(key)
+      .child('votes')
+      .child(currentUser.uid)
+      .set(currentUser.displayName);
+  }
+
+  handleDeSelect(key) {
+    const currentUser = this.props.user;
+    database.ref('/restaurants')
+      .child(key)
+      .child('votes')
+      .child(currentUser.uid)
+      .remove();
+  }
+
   render() {
     const { restaurants } = this.props;
     return (
       <section className="Restaurants">
         {
-          map(restaurants, (restaurant, key) => <Restaurant key={ key } {...restaurant} />)
+          map(restaurants, (restaurant, key) => {
+            return <Restaurant
+              key={key}
+              {...restaurant}
+              handleSelect={ () => this.handleSelect(key)}
+              handleDeselect={ () => this.handleDeSelect(key)}
+            />;
+          })
         }
       </section>
     );
@@ -21,7 +47,7 @@ class Restaurants extends Component {
 }
 
 Restaurants.propTypes = {
-  user: PropTypes,
+  user: PropTypes.object,
   restaurantsRef: PropTypes.object,
   restaurants: PropTypes.object
 };
