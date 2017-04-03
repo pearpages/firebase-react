4e34824dbb09b4903ac5c37aed4907e99d7bc9b6
pearpages <pere@soms.cat>
2017-04-03 19:09:17 +0200

    right firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyD7gX2lL0aQ3-xqKzmHt6DAOPsa_Devymc",
-    authDomain: "lunch-rush-c4626.firebaseapp.com",
-    databaseURL: "https://lunch-rush-c4626.firebaseio.com",
-    projectId: "lunch-rush-c4626",
-    storageBucket: "lunch-rush-c4626.appspot.com",
-    messagingSenderId: "1079065472770"
+    apiKey: "AIzaSyAq_fKyOBA7ANhwVmDJGWKFtGUC20XmZ1o",
+    authDomain: "social-animals-88b4f.firebaseapp.com",
+    databaseURL: "https://social-animals-88b4f.firebaseio.com",
+    projectId: "social-animals-88b4f",
+    storageBucket: "social-animals-88b4f.appspot.com",
+    messagingSenderId: "631846232229"
 };
 
 firebase.initializeApp(config);
