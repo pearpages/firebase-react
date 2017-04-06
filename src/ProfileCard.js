1cccc6cb0f9f8891ed984a4447a82ba1bf2d095b
pearpages <pere@soms.cat>
2017-04-06 10:00:11 +0200

    storing files wip

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -6,11 +6,29 @@ import './ProfileCard.css';
 class ProfileCard extends Component {
   constructor(props) {
     super(props);
+
+    this.storageRef = storage.ref('/user-images').child(props.uid);
+    this.userRef = database.ref('/users').child(props.uid);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    const file = event.target.files[0];
+    const uploadTask = this.storageRef.child(file.name).put(file, {contentType: file.type});
+
+    uploadTask.then((snapshot) => { this.userRef.child('photoURL').set(snapshot.downloadURL)});
   }
 
   render() {
+    const { displayName, photoURL} = this.props.user;
     return (
       <article className="ProfileCard">
+        <img className="ProfileCard--photo" src={ photoURL} />
+        <h3>{ displayName }</h3>
+        <FileInput accept=".png,.gif,.jpg"
+        placeholder="Select an image"
+        onChange={this.handleSubmit}
+        />
       </article>
     );
   }
